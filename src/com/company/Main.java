00c937f239bb9c419d1d33648070d5486759be6c
VincentMaes26 <32563747+VincentMaes26@users.noreply.github.com>
2018-04-03 16:18:56 +0200

    Fixed & added methods to the DALPerson class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,29 @@
 package com.company;
 
 import com.company.dal.DALConnection;
+import com.company.dal.DALPerson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter the ID of the teacher: ");
+            int iD = parseInt(br.readLine()) ;
+
             DALConnection.getInstance().connect();
 
+            String personName = DALPerson.getInstance().getPersonName(iD);
+            String personEmail = DALPerson.getInstance().getPersonEmail(iD);
+            System.out.println("The name of teacher with ID " + iD + ": " + personName);
+            System.out.println("The email of teacher with ID " + personName + ": " + personEmail);
         }
         catch(Exception e){
             System.out.println(e.getMessage());
--- src/com/company/dal/DALPerson.java
+++ src/com/company/dal/DALPerson.java
@@ -1,12 +1,53 @@
 package com.company.dal;
 
+import java.io.IOException;
+import java.sql.*;
+
 public class DALPerson {
+
     private static DALPerson dalPerson;
 
-    public DALPerson getInstance(){
+    public DALPerson() {
+    }
+
+    public static DALPerson getInstance(){
         if(dalPerson==null)
             dalPerson = new DALPerson();
 
         return dalPerson;
+    }
+
+    public String getPersonName(int personID) throws SQLException, IOException {
+        String personName = "";
+        PreparedStatement personStmt = null;
+        String selectSQL = "SELECT name FROM LECTURERS Where id = ?";
+
+        personStmt = DALConnection.getInstance().getConnection().prepareStatement(selectSQL);
+        personStmt.setInt(1, personID);
+        ResultSet rs = personStmt.executeQuery();
+
+        if(!rs.wasNull()) {
+            rs.first();
+            personName = rs.getString(1);
+        }
+
+        return personName;
+    }
+
+    public String getPersonEmail(int personID) throws SQLException, IOException {
+        String personEmail = "";
+        PreparedStatement personStmt = null;
+        String selectSQL = "SELECT email FROM LECTURERS Where id = ?";
+
+        personStmt = DALConnection.getInstance().getConnection().prepareStatement(selectSQL);
+        personStmt.setInt(1, personID);
+        ResultSet rs = personStmt.executeQuery();
+
+        if(!rs.wasNull()) {
+            rs.first();
+            personEmail = rs.getString("email");
+        }
+
+        return personEmail;
     }
 }
